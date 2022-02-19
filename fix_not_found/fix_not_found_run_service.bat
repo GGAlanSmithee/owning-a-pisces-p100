7f7450c926065c0787e9689637b4cfcbe87d1379
Alan Smithee <ggnore.alan.smithee@gmail.com>
2022-02-19 09:23:36 +0100

    actually start the service

--- fix_not_found/fix_not_found_run_service.bat
+++ fix_not_found/fix_not_found_run_service.bat
@@ -2,6 +2,7 @@
 
 sudo systemctl stop fix_not_found.service
 sudo systemctl disable fix_not_found.service
+sudo rm -f /home/pi/hnt/miner/log/fix_not_found.log
+sudo systemctl start fix_not_found.service
 sudo rm -f /lib/systemd/system/fix_not_found.service
 sudo rm -f /home/pi/hnt/miner/log/fix_not_found.py
-sudo rm -f /home/pi/hnt/miner/log/fix_not_found.log
