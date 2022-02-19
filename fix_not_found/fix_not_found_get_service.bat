65f27e813c454444c5471c7170c0caa22702d05c
Alan Smithee <ggnore.alan.smithee@gmail.com>
2022-02-19 09:34:15 +0100

    fix systemctl related code and add install script

--- fix_not_found/fix_not_found_get_service.bat
+++ fix_not_found/fix_not_found_get_service.bat
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# cleanup old files
+sudo rm -f /home/pi/hnt/miner/log/fix_not_found.log
+sudo rm -f /lib/systemd/system/fix_not_found.service
+sudo rm -f /home/pi/hnt/miner/log/fix_not_found.py
+
+# fetch new files
 wget https://raw.githubusercontent.com/GGAlanSmithee/owning-a-pisces-p100/main/fix_not_found/fix_not_found.service -O /lib/systemd/system/fix_not_found.service
 wget https://raw.githubusercontent.com/GGAlanSmithee/owning-a-pisces-p100/main/fix_not_found/fix_not_found.py -O /home/pi/hnt/miner/log/fix_not_found.py
 sudo chmod 644 /lib/systemd/system/fix_not_found.service
--- fix_not_found/fix_not_found_run_service.bat
+++ fix_not_found/fix_not_found_run_service.bat
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# run service
+
 sudo systemctl daemon-reload
-sudo systemctl stop fix_not_found.service
 sudo systemctl disable fix_not_found.service
-sudo rm -f /home/pi/hnt/miner/log/fix_not_found.log
+sudo systemctl stop fix_not_found.service
 sudo systemctl start fix_not_found.service
-sudo rm -f /lib/systemd/system/fix_not_found.service
-sudo rm -f /home/pi/hnt/miner/log/fix_not_found.py
+sudo systemctl enable fix_not_found.service
