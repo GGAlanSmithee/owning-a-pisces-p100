8adf3d6a4249f33ded25eec64a3c958ed3f4c081
Alan Smithee <ggnore.alan.smithee@gmail.com>
2022-02-19 09:13:06 +0100

    only remove files if they exist

--- fix_not_found/fix_not_found_run_service.bat
+++ fix_not_found/fix_not_found_run_service.bat
@@ -2,6 +2,6 @@
 
 sudo systemctl stop fix_not_found.service
 sudo systemctl disable fix_not_found.service
-sudo rm /lib/systemd/system/fix_not_found.service
-sudo rm /home/pi/hnt/miner/log/fix_not_found.py
-sudo rm /home/pi/hnt/miner/log/fix_not_found.log
+sudo rm -f /lib/systemd/system/fix_not_found.service
+sudo rm -f /home/pi/hnt/miner/log/fix_not_found.py
+sudo rm -f /home/pi/hnt/miner/log/fix_not_found.log
