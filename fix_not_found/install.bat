9c94b43b0920ecff51db116adc83fb5e643d1ce7
Alan Smithee <ggnore.alan.smithee@gmail.com>
2022-02-19 09:35:39 +0100

    bat > sh

--- fix_not_found/install.bat
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# get and run the service
-
-wget -O fix_not_found_get_service.bat https://raw.githubusercontent.com/GGAlanSmithee/owning-a-pisces-p100/main/fix_not_found/fix_not_found_get_service.bat; chmod a+x fix_not_found_get_service.bat
-wget -O fix_not_found_run_service.bat https://raw.githubusercontent.com/GGAlanSmithee/owning-a-pisces-p100/main/fix_not_found/fix_not_found_run_service.bat; chmod a+x fix_not_found_run_service.bat
-sudo ./fix_not_found_get_service.bat
-sudo ./fix_not_found_run_service.bat
---
+++ fix_not_found/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# get and run the service
+
+wget -O fix_not_found_get_service.sh https://raw.githubusercontent.com/GGAlanSmithee/owning-a-pisces-p100/main/fix_not_found/fix_not_found_get_service.sh; chmod a+x fix_not_found_get_service.sh
+wget -O fix_not_found_run_service.sh https://raw.githubusercontent.com/GGAlanSmithee/owning-a-pisces-p100/main/fix_not_found/fix_not_found_run_service.sh; chmod a+x fix_not_found_run_service.sh
+sudo ./fix_not_found_get_service.sh
+sudo ./fix_not_found_run_service.sh
