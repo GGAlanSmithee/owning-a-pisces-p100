6ba4e238c63045b1fb9130b6d02a04e7fba1e3aa
Alan Smithee <ggnore.alan.smithee@gmail.com>
2022-02-19 11:25:00 +0100

    remove unnecesart stop and disable

--- fix_not_found/fix_not_found_run_service.sh
+++ fix_not_found/fix_not_found_run_service.sh
@@ -3,7 +3,5 @@
 # run service
 
 sudo systemctl daemon-reload
-sudo systemctl disable fix_not_found.service
-sudo systemctl stop fix_not_found.service
 sudo systemctl start fix_not_found.service
 sudo systemctl enable fix_not_found.service
