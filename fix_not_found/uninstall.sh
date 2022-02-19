ffc9cb100fcd040a6dcddf892a915aaa7a817917
herrtakacs <98314948+herrtakacs@users.noreply.github.com>
2022-02-19 11:50:38 +0100

    Create uninstall.sh

---
+++ fix_not_found/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#clean up everything
+
+sudo systemctl stop fix_not_found.service
+sudo systemctl disable fix_not_found.service
+sudo rm /lib/systemd/system/fix_not_found.service
+sudo rm /home/pi/hnt/miner/log/fix_not_found.py
+sudo rm /home/pi/hnt/miner/log/fix_not_found.log
