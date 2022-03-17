63679e7c95ee72c81b4fcdcd44e3681f4ae15969
Alan Smithee <ggnore.alan.smithee@gmail.com>
2022-03-17 08:39:39 +0100

    fix script path

--- display_miner_stats_on_ssh_login/install.sh
+++ display_miner_stats_on_ssh_login/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # get and save the login script
-wget -O fix_not_found_get_service.sh https://raw.githubusercontent.com/GGAlanSmithee/owning-a-pisces-p100/main/display_miner_stats_on_ssh_login/login_script.sh; chmod a+x /etc/profile.d/login_script.sh
+wget -O /etc/profile.d/login_script.sh https://raw.githubusercontent.com/GGAlanSmithee/owning-a-pisces-p100/main/display_miner_stats_on_ssh_login/login_script.sh; chmod a+x /etc/profile.d/login_script.sh
